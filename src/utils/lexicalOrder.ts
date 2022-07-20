b0c9c7f033f818d71d42ba1a9bc9af6338c194aa
鱼骨头 <1220253798@qq.com>
2022-07-20 16:05:43 +0800

    1

--- src/utils/lexicalOrder.ts
+++ src/utils/lexicalOrder.ts
@@ -4,37 +4,17 @@
  * @desc https://leetcode.cn/problems/lexicographical-numbers/
  */
 export const lexicalOrder = function (n: number) {
-  console.time('lexicalOrder')
+  const nums = new Array(n).fill(1).map((x, y) => y + 1).sort((a, b) => {
+    console.log(a, b)
 
-  const arr = []
-  for (let i = 1; i <= n; i++) {
-    if (i.toString().length === 1) {
-      arr.push([i])
-    }
-    else {
-      const str = i.toString()
-      const index = parseInt(str[0]) - 1
-      const ca = arr[index]
-      for (let idx = 0; idx < ca.length; idx++) {
-        const element = ca[idx]
-        const length = Math.max(str.toString().length, element.toString().length)
-        const aStr = str.toString().padEnd(length, '0')
-        const bStr = element.toString().padEnd(length, '0')
-        if (aStr >= bStr) {
-          if()
-          arr[index] = arr[index].splice(idx, 0, i)
-          continue
-        }
-      }
-    }
-  }
-  const res = arr.flat(1)
-  console.timeEnd('lexicalOrder')
-  return res
+    return a - b
+  })
+  return nums
 }
 
 export const lexicalOrder1 = function (n: number) {
-  const arr = Array.from({ length: n }, (_, i) => i + 1)
+  const arr = Array.from({ length: n }, (_, i) => i + 1).sort()
+  return arr
   const res = arr.sort((a, b) => {
     const length = Math.max(a.toString().length, b.toString().length)
     const aStr = a.toString().padEnd(length, '0')
@@ -52,7 +32,7 @@ if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
 
   it('lexicalOrder', () => {
-    expect(lexicalOrder(23489)).toMatchInlineSnapshot(`
+    expect(lexicalOrder(20)).toMatchInlineSnapshot(`
       [
         1,
         2,
@@ -63,6 +43,17 @@ if (import.meta.vitest) {
         7,
         8,
         9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
       ]
     `)
   })
